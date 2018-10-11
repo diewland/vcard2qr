f59401e5795f58f200b3205f5e68f452e72295fc
diewland <diewland@gmail.com>
2018-10-11 14:54:32 +0700

    support utf-8

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 var cardTemplate = {
   v2: `BEGIN:VCARD
 VERSION:2.1
-N:Gump;Forrest;;Mr.
-FN:Forrest Gump
-ORG:Bubba Gump Shrimp Co.
-TITLE:Shrimp Man
-TEL;WORK;VOICE:(111) 555-1212
-TEL;HOME;VOICE:(404) 555-1212
-ADR;WORK;PREF:;;100 Waters Edge;Baytown;LA;30314;United States of America
-ADR;HOME:;;42 Plantation St.;Baytown;LA;30314;United States of America
-EMAIL;WORK;INTERNET:forrestgump@example.com
+N:แรงไม่มี;แข็ง;;นาย
+FN:แข็ง แรงไม่มี
+ORG:บริษัท แข็งแรงจริงจริง จำกัด
+TITLE:มิสเตอร์ แข็งแรง
+TEL;WORK;VOICE:0898765432
+TEL;HOME;VOICE:0812345678
+ADR;WORK;PREF:;;991 สยามพารากอน;ปทุมวัน;กรุงเทพ;10330;ไทย
+EMAIL;WORK;INTERNET:really@strongbody.com
 END:VCARD`,
   v3: `BEGIN:VCARD
 VERSION:3.0
